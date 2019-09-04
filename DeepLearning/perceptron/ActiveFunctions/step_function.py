13f6d1ccfabe169ee4ea2e4d6e682999e81c0abf
whybfq <13588119112@163.com>
2019-09-04 21:30:59 +0700

    Update step_function.py

--- DeepLearning/perceptron/ActiveFunctions/step_function.py
+++ DeepLearning/perceptron/ActiveFunctions/step_function.py
@@ -1,6 +1,10 @@
+import numpy as np
+import matplotlib.pylab as plt
+
 def step_function(x):
     return np.array(x > 0, dtype=np.int)
 
+# np.arange(-5.0, 5.0, 0.1)在−5.0到5.0的范围内，以0.1为单位，生成 NumPy数组([-5.0, -4.9, ���, 4.9])
 X = np.arange(-5.0, 5.0, 0.1)
 Y = step_function(X)
 plt.plot(X, Y)
